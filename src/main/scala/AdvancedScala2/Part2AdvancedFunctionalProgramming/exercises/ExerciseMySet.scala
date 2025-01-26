b152a192cc92f7dc51e7cf3abf368d5efb80eeee
Wasif Siddique <wsiddique@axon.com>
2025-01-26 01:48:20 -0800

    update comments in MySet

--- src/main/scala/AdvancedScala2/Part2AdvancedFunctionalProgramming/exercises/ExerciseMySet.scala
+++ src/main/scala/AdvancedScala2/Part2AdvancedFunctionalProgramming/exercises/ExerciseMySet.scala
@@ -1,11 +1,13 @@
 package AdvancedScala2.Part2AdvancedFunctionalProgramming.exercises
 
-
 trait ExerciseMySet[A] extends (A => Boolean) {
 
   def contains(elem: A): Boolean
+
   def +(elem: A): ExerciseMySet[A]
   def ++(anotherSet: ExerciseMySet[A]): ExerciseMySet[A]
+  def --(elem: A): ExerciseMySet[A]
+
   def map[B](f: A => B): ExerciseMySet[B]
   def flatMap[B](f: A => ExerciseMySet[B]): ExerciseMySet[B]
   def filter(predicate: A => Boolean): ExerciseMySet[A]
@@ -15,7 +17,6 @@ trait ExerciseMySet[A] extends (A => Boolean) {
     contains(elem)
   }
 
-  def remove(elem: A): ExerciseMySet[A]
   def intersect(anotherSet: ExerciseMySet[A]): ExerciseMySet[A]
   def difference(anotherSet: ExerciseMySet[A]): ExerciseMySet[A]
 
@@ -25,10 +26,86 @@ trait ExerciseMySet[A] extends (A => Boolean) {
 class ExerciseEmpty[A] extends ExerciseMySet[A] {
 
   /** Exercise: implement a functional set
-   */
+    */
   override def contains(elem: A): Boolean = false
 
-  override def +(elem: A): ExerciseMySet[A] = new ExerciseMySet[A] {}
+  override def +(elem: A): ExerciseMySet[A] =
+    new ExerciseNonEmpty[A](elem, this)
+
+  override def ++(anotherSet: ExerciseMySet[A]): ExerciseMySet[A] = anotherSet
+
+  override def --(elem: A): ExerciseMySet[A] = this
+
+  override def map[B](f: A => B): ExerciseMySet[B] = new ExerciseEmpty[B]
+
+  override def flatMap[B](f: A => ExerciseMySet[B]): ExerciseMySet[B] =
+    new ExerciseEmpty[B]
+
+  override def filter(predicate: A => Boolean): ExerciseMySet[A] = this
+
+  override def foreach(f: A => Unit): Unit = ()
+
+
+  override def intersect(anotherSet: ExerciseMySet[A]): ExerciseMySet[A] = this
+
+  override def difference(anotherSet: ExerciseMySet[A]): ExerciseMySet[A] = this
+
+  /** Exercise: Implement a unary_! == Negation of a set
+    */
+  override def unary_! : ExerciseMySet[A] = ???
+}
+
+/** property based set: all the elements of type A that satisfy this predicate,  could be infinte number of these elements
+  * @param property it's a boolean function that takes an element of type A and returns a boolean
+  * @tparam A
+  */
+
+class ExercisePropertyBasedSet[A](property: A => Boolean)
+    extends ExerciseMySet[A] {
+
+  override def contains(elem: A): Boolean = property(elem)
+
+  // the math is, return a new property set where element satisfies property
+  override def +(elem: A): ExerciseMySet[A] = new ExercisePropertyBasedSet[A](x =>
+    property(x) || x == elem
+  )
+
+  override def ++(anotherSet: ExerciseMySet[A]): ExerciseMySet[A] =
+    new ExercisePropertyBasedSet[A](x => property(x) || anotherSet.contains(x))
+
+  override def --(elem: A): ExerciseMySet[A] = filter(x => x != elem)
+
+  // For a partial function, the map method is not implemented
+  override def map[B](f: A => B): ExerciseMySet[B] = properlyFail
+
+  // For a partial function, the flatMap method is not implemented
+  override def flatMap[B](f: A => ExerciseMySet[B]): ExerciseMySet[B] = properlyFail
+
+  // the math is, if the property is true for the element, then the set is the same as the property and the predicate
+  override def filter(predicate: A => Boolean): ExerciseMySet[A] = new ExercisePropertyBasedSet[A](
+    x => property(x) && predicate(x)
+  )
+  // For a partial function, the foreach method is not implemented
+  override def foreach(f: A => Unit): Unit = properlyFail
+
+  // here the math is, filter to get elements that do not math with passed in element to remove
+  override def --(elem: A): ExerciseMySet[A] = filter(x => x != elem)
+
+  override def intersect(anotherSet: ExerciseMySet[A]): ExerciseMySet[A] = filter(anotherSet)
+
+  override def difference(anotherSet: ExerciseMySet[A]): ExerciseMySet[A] = filter(x => !anotherSet(x))
+
+  override def unary_! : ExerciseMySet[A] =
+
+  def properlyFail = throw new RuntimeException("This is a failure from exercise property based set")
+}
+
+class ExerciseNonEmpty[A](head: A, tail: ExerciseMySet[A])
+    extends ExerciseMySet[A] {
+
+  override def contains(elem: A): Boolean = ???
+
+  override def +(elem: A): ExerciseMySet[A] = ???
 
   override def ++(anotherSet: ExerciseMySet[A]): ExerciseMySet[A] = ???
 
@@ -40,18 +117,11 @@ class ExerciseEmpty[A] extends ExerciseMySet[A] {
 
   override def foreach(f: A => Unit): Unit = ???
 
-  /** Exercise:
-   * remove an element
-   * intersection with another set
-   * difference between two sets
-   */
   override def remove(elem: A): ExerciseMySet[A] = ???
 
   override def intersect(anotherSet: ExerciseMySet[A]): ExerciseMySet[A] = ???
 
   override def difference(anotherSet: ExerciseMySet[A]): ExerciseMySet[A] = ???
 
-  /** Exercise: Implement a unary_! == Negation of a set
-   */
   override def unary_! : ExerciseMySet[A] = ???
 }
--- src/main/scala/AdvancedScala2/Part2AdvancedFunctionalProgramming/exercises/MySet.scala
+++ src/main/scala/AdvancedScala2/Part2AdvancedFunctionalProgramming/exercises/MySet.scala
@@ -59,14 +59,29 @@ class EmptySet[A] extends MySet[A] {
 
 /** property based set: all the elements of type A that satisfy this predicate,  could be infinte number of these elements
   * { x in A | property(x) }
+  *  property based set: all the elements of type A that satisfy this predicate,  could be infinte number of these elements
+  *  @param property it's a boolean function that takes an element of type A and returns a boolean
   */
 
 class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
 
   def contains(elem: A): Boolean = property(elem)
 
-  // if (x in A | propery(x)) + elem = { x in A | property(x) || x == elem }
+  /** This method adds an element to the set by creating a new ExercisePropertyBasedSet with an updated property function.
+    * The new property function returns true if the element satisfies the original property or if it is equal to the element being added.
+    * @param elem element type anything A to be added
+    * @return new PropertyBasedSet with added element
+    */
+
+  // if (x in A | property(x)) + elem = { x in A | property(x) || x == elem }
   def +(elem: A): MySet[A] = new PropertyBasedSet[A](x =>
+    /** This line creates a new instance of ExercisePropertyBasedSet with a new property function.
+      * The new property function is a lambda expression x => property(x) || x == elem.
+      * property(x) checks if the element x satisfies the original property.
+      * x == elem checks if the element x is equal to the element being added.
+      * The || operator ensures that the new property function returns true if either condition is met.
+      */
+
     x == elem || property(x)
   )
 
@@ -75,6 +90,13 @@ class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
     * = { x in A | property(x) || anotherSet contains x }
     */
   def ++(anotherSet: MySet[A]): MySet[A] =
+    /** Here, a new instance of PropertyBasedSet is created with a property function
+      * that checks if an element x satisfies either the original set's property or is contained in the anotherSet
+      */
+
+    /** This lambda expression returns true if the element x satisfies the original set's property (property(x))
+      * or if x is contained in anotherSet (anotherSet.contains(x)
+      */
     new PropertyBasedSet[A](x => property(x) || anotherSet.contains(x))
 
   /** mapping on a infinite set doesn't work, as we don't know if the
@@ -83,15 +105,26 @@ class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
   def map[B](f: A => B): MySet[B] = politelyFail
   def flatMap[B](f: A => MySet[B]): MySet[B] = politelyFail
 
+  /** here we are filtering the set based on the predicate and the property
+    */
   def filter(predicate: A => Boolean): MySet[A] = new PropertyBasedSet[A](x =>
     property(x) && predicate(x)
   )
   def foreach(f: A => Unit): Unit = politelyFail
 
+  /** if (x in A | property(x)) -- elem = { x in A | property(x) && x != elem }
+    * if element is in the set, then we remove it by creating a new PropertyBasedSet
+    * with a property function and a lambda expression that checks if an element x satisfies the original property and is not equal to the element being removed.
+    */
   def remove(elem: A): MySet[A] = filter(x => x != elem)
   def intersect(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
   def difference(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
 
+  /** Negation of a set
+    * { x in A | property(x) } => { x in A | !property(x) }
+    * @return a new PropertyBasedSet with a property function satisfying condition x not in this set
+    * that returns the elements not in this set
+    */
   def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x))
 
   def politelyFail = throw new IllegalArgumentException(
@@ -100,12 +133,29 @@ class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
 }
 
 class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
-  def contains(elem: A): Boolean =
+
+  /** @param elem
+    * @return true or false, based on whether the element is in the set
+    * this function is looking at head == element and returns true, otherwise looks at remaining tail
+    */
+  def contains(elem: A): Boolean = {
     elem == head || tail.contains(elem)
+  }
+
+  /** @param elem
+    * @return new Set with added element
+    * this function first checks if element is already in set, if so then returns this
+    * otherwise creates a new NonEmptySet with head as element and tail as this
+    */
   def +(elem: A): MySet[A] = {
     if (this.contains(elem)) this //set doesn't allow duplicate values
     else new NonEmptySet[A](head = elem, tail = this)
   }
+
+  /** @param anotherSet
+    * @return new set added with this set
+    * this function recursively calls tail.++(anotherSet) and adds head to the result
+    */
   def ++(anotherSet: MySet[A]): MySet[A] = {
     tail ++ anotherSet + head //recursion + polymorphism
 
@@ -118,13 +168,29 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
       */
   }
 
+  /** @param f function to map that takes A and returns a new element type B
+    * @tparam B return type
+    * @return a new Set with mapped elements
+    * this function recursively calls tail.map(f) and adds head to the result
+    */
   def map[B](f: A => B): MySet[B] = (tail map f) + f(head)
+
+  /** @param f function to map that takes A and returns a new Set of element type B
+    * @tparam B return type
+    * @return a new Set with mapped elements
+    * this function recursively calls tail.flatMap(f) and then ++ adds f(head) to the result
+    */
   def flatMap[B](f: A => MySet[B]): MySet[B] = (tail flatMap f) ++ f(head)
   def filter(predicate: A => Boolean): MySet[A] = {
     val filteredTail = tail filter predicate
     if (predicate(head)) filteredTail + head
     else filteredTail
   }
+
+  /** @param f a function that takes A and returns Unit
+    *  in this function, first the function is applied to the head,
+    *  and then tail.foreach(f) is recursively called
+    */
   def foreach(f: A => Unit): Unit = {
     f(head)
     tail.foreach(f) // or tail foreach f
@@ -139,7 +205,11 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
     if (head == elem) tail
     else tail.remove(elem) + head
   }
-  //def intersect(anotherSet: MySet[A]): MySet[A] = filter(x => anotherSet.contains(x))
+
+  /** this is a simpler way to do intersect, where filter is called on anotherSet.contains() function
+    */
+  def basicIntersect(anotherSet: MySet[A]): MySet[A] =
+    filter(x => anotherSet.contains(x))
 
   /** simpler intersect
     * since apply and contains are all and the same in MySet, we can call filter on anotherSet
@@ -149,10 +219,20 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
     */
   def intersect(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
 
+  /** Negation of a set
+    *
+    * @return a new MySet with a where elements not in this set
+    * In this function, a new PropertyBasedSet is created with a property function that checks
+    * if an element x is not in this set -> !this.contains(x)
+    */
   def unary_! : MySet[A] = {
     new PropertyBasedSet[A](x => !this.contains(x))
   }
 
+  /** @param anotherSet
+    * @return a new Set with elements that are not in anotherSet
+    * In this function, we filter elements in this set, where the element is not in anotherSet
+    */
   def difference(anotherSet: MySet[A]): MySet[A] =
     filter(x => !anotherSet.contains(x))
 }
